2c32824850af6ec0e5f122217be02ff1531d6e9c
robinsonsilva1 <robinson.silva@seidor.com>
2025-07-02 13:21:27 -0400

    Corrige definición de get_salesforce_token

---
+++ salesforce_auth.py
@@ -0,0 +1,17 @@
+import requests
+import os
+
+def get_salesforce_token():
+    url = "https://login.salesforce.com/services/oauth2/token"
+    payload = {
+        'grant_type': 'password',
+        'client_id': os.environ['CLIENT_ID'],
+        'client_secret': os.environ['CLIENT_SECRET'],
+        'username': os.environ['SF_USERNAME'],
+        'password': os.environ['SF_PASSWORD'] + os.environ['SF_SECURITY_TOKEN']
+    }
+
+    response = requests.post(url, data=payload)
+    response.raise_for_status()
+    data = response.json()
+    return data['access_token'], data['instance_url']
